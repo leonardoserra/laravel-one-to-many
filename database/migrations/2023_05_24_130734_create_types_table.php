7b6bf035ea0f0cdb8858b85b4bc7ebd3691dcd63
leonardoserra <serra.leonardo92@gmail.com>
2023-05-24 15:16:19 +0200

    fatta migrazione tabella  types

--- database/migrations/2023_05_24_130734_create_types_table.php
+++ database/migrations/2023_05_24_130734_create_types_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('types', function (Blueprint $table) {
             $table->id();
+            $table->string('type_name',50)->unique;
+            $table->string('slug')->unique;
             $table->timestamps();
         });
     }
