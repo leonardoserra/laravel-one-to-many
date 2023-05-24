cdb0ae1d37eea23ab75a93b174c9f2db20707aec
leonardoserra <serra.leonardo92@gmail.com>
2023-05-24 15:34:58 +0200

    creto TypeSeedere e popolata tabella types

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +17,13 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = ['Science', 'Economy', 'Sport', 'Art', 'Music', 'History', 'Programming'];
+        foreach($types as $type){
+
+            $newType = new Type();
+            $newType->type_name = $type;
+            $newType->slug = Str::slug($type, '-');
+            $newType->save();
+        };
     }
 }
